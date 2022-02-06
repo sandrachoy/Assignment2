f5d2d6f94abc898836902d6dbdfcbb30a4cd2ddc
Sandra Choy <s10175863@connect.np.edu.sg>
2022-02-07 03:47:50 +0800

    push

--- setup.sql
+++ setup.sql
@@ -4,5 +4,5 @@ USE activities_groups;
 
 CREATE TABLE Conversations (ConversationID varchar (10) NOT NULL PRIMARY KEY, PosterName varchar(30) NOT NULL, PostContent varchar(50) NOT NULL, PostDate date NOT NULL, PostTime time NOT NULL); 
 
-CREATE TABLE Replies (ReplyID int NOT NULL PRIMARY KEY, PosterName varchar(30) NOT NULL, PostContent varchar(50) NOT NULL, PostDate date NOT NULL, PostTime time NOT NULL, ConversationID varchar (5) NOT NULL); 
+CREATE TABLE Replies (ReplyID int NOT NULL PRIMARY KEY, PosterName varchar(30) NOT NULL, PostContent varchar(50) NOT NULL, PostDate date NOT NULL, PostTime time NOT NULL, ConversationID varchar (10) NOT NULL); 
 
